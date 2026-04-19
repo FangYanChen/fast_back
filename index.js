dfa5492ec3c2954aa34fb3b2e89f2fb2fe58091d
FangYanChen <aidarovaidarova@gmail.com>
2026-04-19 12:02:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Backend is working 🚀");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from API" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
